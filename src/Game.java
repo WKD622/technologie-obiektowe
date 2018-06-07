2ce0b28055ff529c4b28e54ee415bd92df512be4
Jakub <you@example.com>
2018-06-07 22:27:40 +0200

    prescribed from uml

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,35 @@
+import java.util.List;
+
 public class Game {
-    String gameName;
-    long gameID;
+    private String gameName;
+    private long gameID;
+    private List<Integer> result;
+    private List<User> observers;
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public List<Integer> getResult() {
+        return result;
+    }
+
+    public void addObserver(User user) {
+        observers.add(user);
+    }
+
+    public void removeObserver(User user){
+        observers.remove(user);
+    }
+
+    public void notifyObservers() {
+        System.out.printf("notify");
+    }
+
+    public void setResult(List<Integer> result) {
+        this.result = result;
+    }
+
 
 }
+
--- src/User.java
+++ src/User.java
@@ -1,2 +1,16 @@
 public class User {
+    /**
+     * niepoprawny camel case dla pół - z dużej litery.
+     */
+    private DeviceType system;
+    private Game ubscribedGame;
+    private long uniqueID;
+
+
+    /**
+     * notify słowo kluczowe, nie można nadpisać.
+     */
+    public void notifyUser(){
+
+    }
 }
