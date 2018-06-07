65d0c5a44bdf6b4bd950a5b7ab71d0fa9e895a98
Jakub <you@example.com>
2018-06-07 22:50:36 +0200

    refactore user

--- src/User.java
+++ src/User.java
@@ -2,22 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    /**
-     * niepoprawny camel case dla pól - z dużej litery.
-     */
+
     private DeviceType system;
     private List<Game> subscribedGames = new ArrayList<>();
     private long uniqueID;
 
 
-    /**
-     * notify słowo kluczowe, nie można nadpisać.
-     */
-    public void notifyUser(Game game){
-        System.out.printf("Hello " , uniqueID,". You are notified by ", game, ".");
+    public void notifyUser(Game game) {
+        System.out.println("Hello " + uniqueID + ". You are notified by " + game.toString() + ".");
     }
 
-    public void addGame(Game game){
+    public void addGame(Game game) {
         subscribedGames.add(game);
     }
 
